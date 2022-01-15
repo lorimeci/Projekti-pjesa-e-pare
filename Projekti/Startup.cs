870a83a6e2b4295bcc97f8a5cd3235332ad46525
Lorena Meci <lorena_meci.fshnstudent@unitir.edu.al>
2022-01-15 22:26:09 +0100

    Add project files.

---
+++ Projekti/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Projekti.Startup))]
+namespace Projekti
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
